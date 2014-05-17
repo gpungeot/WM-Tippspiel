34198ccf48c8aa4c52a886ff244fc4c38dd74456
Tomtom <tomtom_dev@yahoo.fr>
2014-05-17 14:58:09 +0200

    Analytics

--- templates/startPage.php
+++ templates/startPage.php
@@ -38,7 +38,7 @@ print("</div>");
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-50952108-1', 'brevesdebureau.fr');
+  ga('create', 'UA-51096440-1', 'brevesdebureau.fr');
   ga('send', 'pageview');
 
 </script>
